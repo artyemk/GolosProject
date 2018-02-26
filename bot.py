df2967c6e59ce53d236cac65b4e3c7f26147537d
artyemk <kolodaio@gmail.com>
2018-02-26 23:06:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,9 @@
 # In[1]:
 from pymongo import MongoClient
 import pymongo
-import os
-import subprocess
-import threading
-import get_stat
-import sys
-import signal
 import re
 import datetime
 import time
-from logging import log
 import telebot
 from telebot import types
 
@@ -58,61 +51,6 @@ def callbacks(call):
     else:
         bot.send_message(s[0],"Выберите дату:")
         get_stats_msg(s[0])
-
-
-@bot.message_handler(commands=['stats'])
-def get_stats_msg(message):
-    bot.send_message(message, "Анализирую собранную статистику. Пожалуйста подождите...")
-    time.sleep(2)
-    res_popular = ""
-    arr = []
-    count = 0
-    for item in popular:
-        res_popular += str(count) + ") " + str(item[0]) + ": " + str(item[1]) + "\n"
-    res_cash = ""
-    res_votes = ""
-    res_comms = ""
-    
-    sort_cash = []
-    sort_votes = []
-    sort_comms = []
-    for item in stats:
-        temp1 = [item['word'], round(item['avg_cash'],2)]
-        temp2 = [item['word'], round(item['avg_votes'],2)]
-        temp3 = [item['word'], round(item['avg_comms'],2)]
-        sort_cash.append(temp1)
-        sort_votes.append(temp2)
-        sort_comms.append(temp3)
-    sort_cash.sort(reverse = True, key=lambda x: x[1])
-    sort_votes.sort(reverse = True, key=lambda x: x[1])
-    sort_comms.sort(reverse = True, key=lambda x: x[1])
-    count = 0 
-    for item in sort_cash:
-        count += 1
-        res_cash += str(count) + ")" + str(item[0]) + ": " + str(item[1]) + "\n"
-    count = 0
-    for item in sort_votes:
-        count += 1
-        res_votes += str(count) + ")" + str(item[0]) + ": " + str(item[1]) + "\n"
-    count = 0
-    for item in sort_comms:
-        count += 1
-        res_comms += str(count) + ")" + str(item[0]) + ": " + str(item[1]) + "\n"
-
-#    db.save.
-    print(res_cash)
-    print(res_votes)
-    print(res_comms)
-    bot.send_message(message, "Статистика собрана:")
-    bot.send_message(message, template_popular + res_popular)
-    bot.send_message(message, template_mood +
-                         "Позитивных постов: " +str(mood["positive_comments"]) + "\n" +
-                         "Негативных постов: " + str(mood["negative_comments"]) + "\n" +
-                         "Нейтральных постов: " + str(mood["neutral_comments"]) + "\n")
-    bot.send_message(message, template_cash + res_cash)
-    bot.send_message(message, template_votes + res_votes)
-    bot.send_message(message, template_comms + res_comms)
-
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
