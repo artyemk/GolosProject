89688ade7ba23a8b7abcb1ea9c7f8181b7b312c5
artyemk <kolodaio@gmail.com>
2018-02-26 23:55:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ url = "https://api.telegram.org/bot%s/", token
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = types.InlineKeyboardMarkup()
-    btn = types.InlineKeyboardButton(text="Today", callback_data=str(message.chat.id) + "_data")
+    btn = types.InlineKeyboardButton(text="Today", callback_data=str(message.chat.id) + "_today")
     keyboard.add(btn)
     btn1= types.InlineKeyboardButton(text="Choose date(WIP)", callback_data=str(message.chat.id) +"_date")
     keyboard.add(btn1)
@@ -38,11 +38,58 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callbacks(call):
     s = call.data.split("_")
-    if s[1] == "data":
-        bot.send_message(s[0],s[1])
-        db.stats.find_one({"timestamp": str(datetime.now()).split(" ")[0]})
+    if s[1] == "today":
+        stats = db.stats.find_one({"timestamp": str(datetime.now()).split(" ")[0]})
+        bot.send_message(s[0],)
     else:
         bot.send_message(s[0],"Выберите дату:")
+        
+        
+def user_text(message,popular,mood,stats):
+    res_popular = ""
+    arr = []
+    res_cash = ""
+    res_votes = ""
+    res_comms = ""
+    sort_cash = []
+    sort_votes = []
+    sort_comms = []
+    count = 0
+    for item in popular:
+        res_popular += str(count) + ") " + str(item[0]) + ": " + str(item[1]) + "\n"
+        count += 1
+    for item in stats:
+        temp1 = [item['word'], round(item['avg_cash'],2)]
+        temp2 = [item['word'], round(item['avg_votes'],2)]
+        temp3 = [item['word'], round(item['avg_comms'],2)]
+        sort_cash.append(temp1)
+        sort_votes.append(temp2)
+        sort_comms.append(temp3)
+    sort_cash.sort(reverse = True, key=lambda x: x[1])
+    sort_votes.sort(reverse = True, key=lambda x: x[1])
+    sort_comms.sort(reverse = True, key=lambda x: x[1])
+    count = 0 
+    for item in sort_cash:
+        count += 1
+        res_cash += str(count) + ")" + str(item[0]) + ": " + str(item[1]) + "\n"
+    count = 0
+    for item in sort_votes:
+        count += 1
+        res_votes += str(count) + ")" + str(item[0]) + ": " + str(item[1]) + "\n"
+    count = 0
+    for item in sort_comms:
+        count += 1
+        res_comms += str(count) + ")" + str(item[0]) + ": " + str(item[1]) + "\n"
+        
+    bot.send_message(message, "Статистика собрана:")
+    bot.send_message(message, template_popular + res_popular)
+    bot.send_message(message, template_mood +
+                         "Позитивных постов: " +str(mood["positive_comments"]) + "\n" +
+                         "Негативных постов: " + str(mood["negative_comments"]) + "\n" +
+                         "Нейтральных постов: " + str(mood["neutral_comments"]) + "\n")
+    bot.send_message(message, template_cash + res_cash)
+    bot.send_message(message, template_votes + res_votes)
+    bot.send_message(message, template_comms + res_comms)
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
